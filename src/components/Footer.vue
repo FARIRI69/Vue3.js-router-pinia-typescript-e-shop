b7cc62d74fa6e8458d2dada8ff759b6f967facc4
FARID <109043236+FARIRI69@users.noreply.github.com>
2023-06-26 17:48:08 +0200

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="d-flex flex-row justify-content-center align-items-center">
+    <p>Copyright © 2023 - Worn.inc</p>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  background-color: var(--primary-1);
+  color: var(--text-primary-color);
+}
+</style>
